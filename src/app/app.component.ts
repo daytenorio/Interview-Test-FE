d9321112cb44d741ba4659097ca066df0c8bdda6
Day Tenorio <ddstenorio@outlook.com>
2019-06-16 02:16:24 -0400

    listening ESC
    Close tooltips on esc press

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, ViewChild } from "@angular/core";
+import { Button } from "protractor";
+import { ButtonsComponent } from "./buttons/buttons.component";
 
 @Component({
   selector: "app-root",
@@ -7,4 +9,11 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "Interview-Test-FE";
+  @ViewChild(ButtonsComponent) private buttons: ButtonsComponent;
+
+  @HostListener("document:keyup", ["$event"]) handleKeyUp(event: {
+    keyCode: number;
+  }) {
+    if (event.keyCode === 27) this.buttons.hideToolTips();
+  }
 }
