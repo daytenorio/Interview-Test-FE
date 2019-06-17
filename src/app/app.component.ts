d5e8572e0300b876e4d386a5dd0dfa0419a9b3ea
Day Tenorio <ddstenorio@outlook.com>
2019-06-16 21:30:31 -0400

    document:click
    hide tooltip onClick anywehere on the screen but on buttons.
    changed element input to button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener, ViewChild } from "@angular/core";
 import { Button } from "protractor";
 import { ButtonsComponent } from "./buttons/buttons.component";
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 
 @Component({
   selector: "app-root",
@@ -10,10 +11,16 @@ import { ButtonsComponent } from "./buttons/buttons.component";
 export class AppComponent {
   title = "Interview-Test-FE";
   @ViewChild(ButtonsComponent) private buttons: ButtonsComponent;
-
+  constructor(private sanitizer: DomSanitizer) {}
   @HostListener("document:keyup", ["$event"]) handleKeyUp(event: {
     keyCode: number;
   }) {
     if (event.keyCode === 27) this.buttons.hideToolTips();
+  }
+
+  @HostListener("document:click", ["$event.target.localName"]) onClick(
+    targetElement: string
+  ) {
+    if (targetElement !== "button") this.buttons.hideToolTips();
   }
 }
